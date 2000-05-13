670a264470ee3cf7cde1c1615e2ba2e41b3c48d1
Yan Sweitzer <yanster@users.sourceforge.net>
2000-05-13 05:32:48 +0000

    putch needs <conio.h> -- Yanster

--- source/cl_sys_win.c
+++ source/cl_sys_win.c
@@ -54,6 +54,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "fcntl.h"
 #include <limits.h>
 #include <io.h>
+#include <conio.h>
 
 qboolean is_server = false;
 
