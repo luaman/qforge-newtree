7e4a424854104cc0c33bce902715378379a81926
Joseph Carter <knghtbrd@debian.org>
2000-05-14 23:58:57 +0000

    Add the QSG and QF version keys

--- source/sv_main.c
+++ source/sv_main.c
@@ -1381,12 +1381,14 @@ void SV_InitLocal (void)
 	for (i=0 ; i<MAX_MODELS ; i++)
 		sprintf (localmodels[i], "*%i", i);
 
-//	Info_SetValueForStarKey (svs.info, "*qf_version", QF_VERSION,
-//			MAX_SERVERINFO_STRING);
 	Info_SetValueForStarKey (svs.info, "*version", QW_VERSION,
 			MAX_SERVERINFO_STRING);
-//	Info_SetValueForStarKey (svs.info, "*qsg_standard", QSG_STANDARD,
-//			MAX_SERVERINFO_STRING);
+
+	// Brand server as QF, with appropriate QSG standards version  --KB
+	Info_SetValueForStarKey (svs.info, "*qf_version", QF_VERSION,
+			MAX_SERVERINFO_STRING);
+	Info_SetValueForStarKey (svs.info, "*qsg_standard", QSG_STANDARD,
+			MAX_SERVERINFO_STRING);
 
 	// init fraglog stuff
 	svs.logsequence = 1;
