c87243d94c2b5055be42aa69e128ba39aef5ee2d
Marcus Sundberg <mackan@stacken.kth.se>
2000-05-20 03:26:19 +0000

    Arm byte typedef with #ifndef _DEF_BYTE_

--- include/common.h
+++ include/common.h
@@ -24,8 +24,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 //#include "protocol.h" // for struct usercmd_s
 
+#ifndef _DEF_BYTE_
+# define _DEF_BYTE_
 typedef unsigned char 		byte;
-#define _DEF_BYTE_
+#endif
 
 // KJB Undefined true and false defined in SciTech's DEBUG.H header
 #undef true
