96ecbb71bfc9db745c5bdf8317d4cf3f823ea79d
Marcus Sundberg <mackan@stacken.kth.se>
2000-05-21 21:41:46 +0000

    Fix ifdef around pragma.

--- include/anorms.h
+++ include/anorms.h
@@ -17,7 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-#ifdef _WIN32
+#if defined(_WIN32) && !defined(__GNUC__)
 #pragma warning(disable:4305)
 #endif
 {-0.525731, 0.000000, 0.850651}, 
