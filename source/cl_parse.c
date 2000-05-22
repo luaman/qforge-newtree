aec65c68a2059e4f2957445f1422ef2561063124
Anton E. Gavrilov <tonik@vipmail.ru>
2000-05-22 05:00:44 +0000

    Execution of autoexec.cfg on gamedir change is temporarily disabled

--- source/cl_parse.c
+++ source/cl_parse.c
@@ -584,15 +584,29 @@ void CL_ParseServerData (void)
 	//ZOID--run the autoexec.cfg in the gamedir
 	//if it exists
 	if (cflag) {
-		snprintf (fn, sizeof(fn), "%s/%s", com_gamedir, "config.cfg");
+		int cl_warncmd_val = cl_warncmd->value;
+		snprintf(fn, sizeof(fn), "%s/%s", com_gamedir, "config.cfg");
 		if ((f = Qopen(fn, "r")) != NULL) {
 			Qclose(f);
 			Cbuf_AddText ("cl_warncmd 0\n");
-			Cbuf_AddText("exec config.cfg\n");
-			Cbuf_AddText("exec frontend.cfg\n");
-			Cbuf_AddText("exec autoexec.cfg\n");
-			Cbuf_AddText ("cl_warncmd 1\n");
+			Cbuf_AddText ("exec config.cfg\n");
 		}
+		snprintf(fn, sizeof(fn), "%s/%s", com_gamedir, "frontend.cfg");
+		if ((f = Qopen(fn, "r")) != NULL) {
+			Qclose(f);
+			Cbuf_AddText ("cl_warncmd 0\n");
+			Cbuf_AddText ("exec frontend.cfg\n");
+		}
+/*		if (cl_autoexec->value) {
+			snprintf(fn, sizeof(fn), "%s/%s", com_gamedir, "autoexec.cfg");
+			if ((f = Qopen(fn, "r")) != NULL) {
+				Qclose(f);
+				Cbuf_AddText ("cl_warncmd 0\n");
+				Cbuf_AddText ("exec autoexec.cfg\n");
+			}
+		}	*/
+		snprintf(fn,sizeof(fn), "cl_warncmd %d\n", cl_warncmd_val);
+		Cbuf_AddText(fn);
 	}
 
 	// parse player slot, high bit means spectator
