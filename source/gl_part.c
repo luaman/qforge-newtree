2530186c5e3768b4e06a0ff97d3aa14263920501
Bill Currie <bill@taniwha.org>
2000-06-19 23:12:55 +0000

    make r_firecolor more forgiving for old config files. prints a warning if the old second alpha parameter is given, but accepts the other args.

--- source/gl_part.c
+++ source/gl_part.c
@@ -818,7 +818,9 @@ R_FireColor_f(void)
 					r_firecolor_flame[3]);
 		return;
 	}
-	if (Cmd_Argc() !=5) {
+	if (Cmd_Argc() == 6) {
+		Con_Printf ("Warning: obsolete 5th parameter to r_firecolor ignored\n");
+	} else if (Cmd_Argc() !=5) {
 		Con_Printf ("Usage r_firecolor R G B A\n");
 		return;
 	}
