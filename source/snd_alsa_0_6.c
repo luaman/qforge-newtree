ae61fee7239f17e99324e90c99b9f7d7d877e9fe
Bill Currie <bill@taniwha.org>
2000-09-26 18:58:50 +0000

    latest alsa 0.6.x support from nuq

--- source/snd_alsa_0_6.c
+++ source/snd_alsa_0_6.c
@@ -66,7 +66,6 @@
 static int snd_inited;
 
 static snd_pcm_t *pcm_handle;
-static snd_pcm_info_t cinfo;
 static snd_pcm_params_info_t cpinfo;
 static snd_pcm_params_t params;
 static snd_pcm_setup_t setup;
@@ -74,6 +73,10 @@ static snd_pcm_mmap_control_t *mmap_control = NULL;
 static snd_pcm_mmap_status_t *mmap_status = NULL;
 static char *mmap_data = NULL;
 static int card=-1,dev=-1,subdev=-1;;
+
+//XXX ugh, not defined in asoundlib.h
+int snd_pcm_mmap_status(snd_pcm_t *pcm, snd_pcm_mmap_status_t **status);
+int snd_pcm_mmap_control(snd_pcm_t *pcm, snd_pcm_mmap_control_t **control);
 
 int check_card(int card)
 {
@@ -116,7 +119,7 @@ qboolean SNDDMA_Init(void)
 {
 	int rc=0,i;
 	char *err_msg="";
-	int rate,format,bps,stereo,frag_size,frame_size;
+	int rate=-1,format=-1,bps,stereo=-1,frag_size,frame_size;
 	unsigned int mask;
 
 	mask = snd_cards_mask();
@@ -129,6 +132,27 @@ qboolean SNDDMA_Init(void)
 	}
 	if ((i=COM_CheckParm("-snddev"))!=0) {
 		dev=atoi(com_argv[i+1]);
+	}
+	if ((i=COM_CheckParm("-sndbits")) != 0) {
+		i = atoi(com_argv[i+1]);
+		if (i==16) {
+			format = SND_PCM_SFMT_S16_LE;
+		} else if (i==8) {
+			format = SND_PCM_SFMT_U8;
+		} else {
+			Con_Printf("Error: invalid sample bits: %d\n", i);
+			return 0;
+		}
+	}
+	if ((i=COM_CheckParm("-sndspeed")) != 0) {
+		rate = atoi(com_argv[i+1]);
+		if (rate!=44100 && rate!=22050 && rate!=11025) {
+			Con_Printf("Error: invalid sample rate: %d\n", rate);
+			return 0;
+		}
+	}
+	if ((i=COM_CheckParm("-sndmono")) != 0) {
+		stereo=0;
 	}
 	if (card==-1) {
 		for (card=0; card<SND_CARDS; card++) {
@@ -162,29 +186,27 @@ qboolean SNDDMA_Init(void)
 
  dev_openned:
 	Con_Printf("Using card %d, device %d.\n", card, dev);
-	memset(&cinfo, 0, sizeof(cinfo));
-	snd_pcm_info(pcm_handle, &cinfo);
 	memset(&cpinfo, 0, sizeof(cpinfo));
 	snd_pcm_params_info(pcm_handle, &cpinfo);
-	Con_Printf("%08x %08x %08x\n",cinfo.flags,cpinfo.formats,cpinfo.rates);
-	if (cpinfo.rates & SND_PCM_RATE_44100) {
+	Con_Printf("%08x %08x %08x\n",cpinfo.flags,cpinfo.formats,cpinfo.rates);
+	if ((rate==-1 || rate==44100) && cpinfo.rates & SND_PCM_RATE_44100) {
 		rate=44100;
 		frag_size=256;	/* assuming stereo 8 bit */
-	} else if (cpinfo.rates & SND_PCM_RATE_22050) {
+	} else if ((rate==-1 || rate==22050) && cpinfo.rates & SND_PCM_RATE_22050) {
 		rate=22050;
 		frag_size=128;	/* assuming stereo 8 bit */
-	} else if (cpinfo.rates & SND_PCM_RATE_11025) {
+	} else if ((rate==-1 || rate==11025) && cpinfo.rates & SND_PCM_RATE_11025) {
 		rate=11025;
 		frag_size=64;	/* assuming stereo 8 bit */
 	} else {
 		Con_Printf("ALSA: desired rates not supported\n");
 		goto error_2;
 	}
-	if (cpinfo.formats & SND_PCM_FMT_S16_LE) {
+	if ((format==-1 || format==SND_PCM_SFMT_S16_LE) && cpinfo.formats & SND_PCM_FMT_S16_LE) {
 		format=SND_PCM_SFMT_S16_LE;
 		bps=16;
 		frame_size=2;
-	} else if (cpinfo.formats & SND_PCM_FMT_U8) {
+	} else if ((format==-1 || format==SND_PCM_SFMT_U8) && cpinfo.formats & SND_PCM_FMT_U8) {
 		format=SND_PCM_SFMT_U8;
 		bps=8;
 		frame_size=1;
@@ -192,7 +214,8 @@ qboolean SNDDMA_Init(void)
 		Con_Printf("ALSA: desired formats not supported\n");
 		goto error_2;
 	}
-	if (cpinfo.max_channels>=2) {
+	//XXX can't support non-interleaved stereo
+	if (stereo && (cpinfo.flags & SND_PCM_INFO_INTERLEAVED) && cpinfo.max_channels>=2) {
 		stereo=1;
 		frame_size*=2;
 	} else {
@@ -200,38 +223,45 @@ qboolean SNDDMA_Init(void)
 	}
 
 	memset(&params, 0, sizeof(params));
-	params.mode = SND_PCM_MODE_FRAME;
-	params.format.interleave=1;
-	params.format.format=format;
+	//XXX can't support non-interleaved stereo
+	params.xfer_mode = stereo ? SND_PCM_XFER_INTERLEAVED
+							  : SND_PCM_XFER_NONINTERLEAVED;
+	params.format.sfmt=format;
 	params.format.rate=rate;
 	params.format.channels=stereo+1;
-	params.start_mode = SND_PCM_START_GO;
-	params.xrun_mode = SND_PCM_XRUN_RESTART;
+	params.start_mode = SND_PCM_START_EXPLICIT;
+	params.xrun_mode = SND_PCM_XRUN_NONE;
 
 	params.buffer_size = (2<<16) / frame_size;
 	params.frag_size=frag_size;
 	params.avail_min = frag_size;
-	params.align = frag_size;
 
 	params.xrun_max = 1024;
-	params.fill_mode = SND_PCM_FILL_SILENCE;
-	params.fill_max = 1024;
 	params.boundary = params.buffer_size;
 
-	err_msg="audio params";
-	if ((rc=snd_pcm_params(pcm_handle, &params))<0)
-		goto error;
+	while (1) {
+		err_msg="audio params";
+		if ((rc=snd_pcm_params(pcm_handle, &params))<0)
+			goto error;
+
+		memset(&setup, 0, sizeof(setup));
+		err_msg="audio setup";
+		if ((rc=snd_pcm_setup(pcm_handle, &setup))<0)
+			goto error;
+		if (setup.buffer_size == params.buffer_size)
+			break;
+		params.boundary = params.buffer_size = setup.buffer_size;
+	}
 
 	err_msg="audio mmap";
-	if ((rc=snd_pcm_mmap(pcm_handle, &mmap_status, &mmap_control, (void **)&mmap_data))<0)
+	if ((rc=snd_pcm_mmap(pcm_handle, (void**)&mmap_data))<0)
+		goto error;
+	if ((rc=snd_pcm_mmap_status(pcm_handle, &mmap_status))<0)
+		goto error;
+	if ((rc=snd_pcm_mmap_control(pcm_handle, &mmap_control))<0)
 		goto error;
 	err_msg="audio prepare";
 	if ((rc=snd_pcm_prepare(pcm_handle))<0)
-		goto error;
-
-	memset(&setup, 0, sizeof(setup));
-	err_msg="audio setup";
-	if ((rc=snd_pcm_setup(pcm_handle, &setup))<0)
 		goto error;
 
 	shm=&sn;
@@ -240,8 +270,8 @@ qboolean SNDDMA_Init(void)
 	shm->channels=setup.format.channels;
 	shm->submission_chunk=frag_size;			// don't mix less than this #
 	shm->samplepos=0;							// in mono samples
-	shm->samplebits=setup.format.format==SND_PCM_SFMT_S16_LE?16:8;
-	shm->samples=setup.buffer_size*frame_size/(shm->samplebits/8);	// mono samples in buffer
+	shm->samplebits=setup.format.sfmt==SND_PCM_SFMT_S16_LE?16:8;
+	shm->samples=setup.buffer_size*shm->channels;	// mono samples in buffer
 	shm->speed=setup.format.rate;
 	shm->buffer=(unsigned char*)mmap_data;
     Con_Printf("%5d stereo\n", shm->channels - 1);
@@ -266,7 +296,7 @@ int SNDDMA_GetDMAPos(void)
 {
 	size_t hw_ptr;
 	if (!snd_inited) return 0;
-	hw_ptr = snd_pcm_hw_ptr (pcm_handle, 1);
+	hw_ptr = mmap_status->hw_ptr;
 	//printf("%7d %7d\n", mmap_control->appl_ptr, hw_ptr);
 	hw_ptr *= shm->channels;
 	shm->samplepos = hw_ptr;
@@ -297,7 +327,7 @@ void SNDDMA_Submit(void)
 	mmap_control->appl_ptr=mmap_status->hw_ptr+count;
 	switch (mmap_status->state) {
 	case SND_PCM_STATE_PREPARED:
-		if ((rc=snd_pcm_go(pcm_handle))<0) {
+		if ((rc=snd_pcm_start(pcm_handle))<0) {
 			fprintf(stderr, "unable to start playback. %s\n",
 					snd_strerror(rc));
 			exit(1);
