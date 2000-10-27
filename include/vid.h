e6785026416adc9956f91a6fc21ad94c16a28bbe
Jeff Teunissen <deek@d2dc.net>
2000-10-27 10:16:18 +0000

    Mostly whitespace

--- include/vid.h
+++ include/vid.h
@@ -26,30 +26,28 @@
 	$Id$
 */
 
-#ifndef _VID_H
-#define _VID_H
+#ifndef __vid_h_
+#define __vid_h_
 
 #include "qtypes.h"
 
 #define VID_CBITS	6
 #define VID_GRADES	(1 << VID_CBITS)
 
-typedef struct vrect_s
-{
+typedef struct vrect_s {
 	int				x,y,width,height;
 	struct vrect_s	*pnext;
 } vrect_t;
 
-typedef struct
-{
+typedef struct {
 	pixel_t			*buffer;		// invisible buffer
 	pixel_t			*colormap;		// 256 * VID_GRADES size
 	unsigned short	*colormap16;	// 256 * VID_GRADES size
 	int				fullbright;		// index of first fullbright color
-	unsigned int	rowbytes;	// may be > width if displayed in a window
+	unsigned int	rowbytes;		// may be > width if displayed in a window
 	unsigned int	width;		
 	unsigned int	height;
-	float			aspect;		// width / height -- < 0 is taller than wide
+	float			aspect;			// width / height -- < 0 is taller than wide
 	int				numpages;
 	int				recalc_refdef;	// if true, recalc vid-based stuff
 	pixel_t			*conbuffer;
@@ -62,46 +60,45 @@ typedef struct
 									//  NULL
 } viddef_t;
 
-extern	viddef_t	vid;				// global video state
-extern	unsigned short	d_8to16table[256];
-extern	unsigned int	d_8to24table[256];
-extern void (*vid_menudrawfn)(void);
-extern void (*vid_menukeyfn)(int key);
+extern viddef_t 		vid;				// global video state
+extern unsigned short	d_8to16table[256];
+extern unsigned int 	d_8to24table[256];
 extern int scr_width, scr_height;
 
-void	VID_SetPalette (unsigned char *palette);
 // called at startup and after any gamma correction
+void VID_SetPalette (unsigned char *palette);
 
-void	VID_ShiftPalette (unsigned char *palette);
 // called for bonus and pain flashes, and for underwater color changes
+void VID_ShiftPalette (unsigned char *palette);
 
-void	VID_InitCvars (void);
-void	VID_Init (unsigned char *palette);
+void VID_InitCvars (void);
+
 // Called at startup to set up translation tables, takes 256 8 bit RGB values
 // the palette data will go away after the call, so it must be copied off if
 // the video driver will need it again
+void VID_Init (unsigned char *);
 
-void	VID_Shutdown (void);
 // Called at shutdown
+void VID_Shutdown (void);
 
-void	VID_Update (vrect_t *rects);
 // flushes the given rectangles from the view buffer to the screen
+void VID_Update (vrect_t *rects);
 
-int VID_SetMode (int modenum, unsigned char *palette);
 // sets the mode; only used by the Quake engine for resetting to mode 0 (the
 // base mode) on memory allocation failures
+int VID_SetMode (int modenum, unsigned char *palette);
 
-void VID_HandlePause (qboolean pause);
 // called only on Win32, when pause happens, so the mouse can be released
+void VID_HandlePause (qboolean pause);
 
 void VID_LockBuffer (void);
 void VID_UnlockBuffer (void);
 
-qboolean VID_Is8bit(void);
+qboolean VID_Is8bit (void);
 
-void VID_SetCaption(char *text);
+void VID_SetCaption (char *text);
 // used to set window caption
 
 void VID_GetWindowSize (int def_w, int def_h);
 
-#endif // _VID_H
+#endif	// __vid_h_
