54963bfcb4cc1a55c668bebae895d9405f4dd089
Bill Currie <bill@taniwha.org>
2000-10-31 04:16:20 +0000

    vid_fullscreen is meant to be CVAR_ROM, not CVAR_ARCHIVE
    * taniwha programs his black logo logitech wheel mouse with secret instructions

--- source/context_x11.c
+++ source/context_x11.c
@@ -291,8 +291,8 @@ x11_set_vidmode(int width, int height)
 void
 x11_Init_Cvars ()
 {
-	vid_fullscreen = Cvar_Get ("vid_fullscreen", "0", CVAR_ARCHIVE,
-			"Toggles fullscreen game mode");
+	vid_fullscreen = Cvar_Get ("vid_fullscreen", "0", CVAR_ROM,
+							   "Toggles fullscreen game mode");
 }
 
 void
