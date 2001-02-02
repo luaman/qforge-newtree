175f24020d117fc210150ca2247a864b5a01fce3
Ragnvald Maartmann-Moe IV <despair@users.sourceforge.net>
2001-02-02 20:47:31 +0000

    Comment typo fix.

--- source/gl_rlight.c
+++ source/gl_rlight.c
@@ -372,7 +372,7 @@ RecursiveLightPoint (mnode_t *node, vec3_t start, vec3_t end)
 		return r;						// hit something
 
 	if ((back < 0) == side)
-		return -1;						// didn't hit anuthing
+		return -1;						// didn't hit anything
 
 // check for impact on this node
 	VectorCopy (mid, lightspot);
