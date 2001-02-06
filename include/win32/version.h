b443a0254ea13238cd2e60f187bce29f7e002022
Bill Currie <bill@taniwha.org>
2001-02-06 00:29:16 +0000

    rev version for beta7

--- include/win32/version.h
+++ include/win32/version.h
@@ -30,7 +30,7 @@
 /* Version strings */
 #define PACKAGE 	"quakeforge"
 #define PROGRAM 	"QuakeForge"
-#define VERSION 	"0.2.99beta6"
+#define VERSION 	"0.2.99beta7"
 #define QW_VERSION	"2.40"
 #define QSG_VERSION	"2.0"
 
