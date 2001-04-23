8327d1edace9360969d2f6931c1a26ada9baf730
Bill Currie <bill@taniwha.org>
2001-04-23 18:44:56 +0000

    clean up V_CheckGamma a bit

--- source/r_view.c
+++ source/r_view.c
@@ -271,20 +271,12 @@ byte        gammatable[256];			// palette is sent through this
 qboolean
 V_CheckGamma (void)
 {
-	//static float oldbrightness;
-	//static float oldcontrast;
 	static float oldgamma;
 
-	if (oldgamma != vid_gamma->value) {
-		oldgamma = vid_gamma->value;
-		VID_UpdateGamma (vid_gamma);
-	}
-	//if ((brightness->value == oldbrightness) && contrast->value == oldcontrast)
-	//	return false;
-	//oldbrightness = brightness->value;
-	//oldcontrast = contrast->value;
-
-	//BuildGammaTable (brightness->value, contrast->value);
+	if (oldgamma == vid_gamma->value)
+		return false;
+	oldgamma = vid_gamma->value;
+	VID_UpdateGamma (vid_gamma);
 	vid.recalc_refdef = 1;				// force a surface cache flush
 
 	return true;
